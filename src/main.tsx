9ffe8f3f32f5541095940898cdf3fb08c3736d77
PrestigioNEt <prestigedigitalplus@gmail.com>
2025-07-06 00:28:45 -0500

    Add missing src/main.tsx entry file

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // Asegúrate de que este archivo CSS exista si lo usas
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
